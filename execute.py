412eed3c0b96b7f33eb1583dbcf43ebcb38acce0
yida-winUSB <845917401@qq.com>
2018-11-17 16:52:42 +0800

    update follow upsteam

--- execute.py
+++ execute.py
@@ -4,9 +4,20 @@ from models.NMF_model import NMF_model
 import pandas as pd
 import profile
 
+import numpy as np
+
+# print(np.__version__)
+
 data_df = pd.read_csv("Data/train_data.txt", sep='\t', header=-1)
 test_df = pd.read_csv("Data/test_data.txt", sep='\t', header=-1)
 model = NMF_model(data_df, 5)
 model.train()
 
 model.evaluation(test_df)
+
+# best:  [0.39546783625731041, 0.37456140350877132, 0.31491228070175387, 0.41066404399874989, 0.39764448720509904,
+#         0.37044137039933167]
+# epoch
+# 14
+# gen:  [0.39546783625731041, 0.37456140350877132, 0.31491228070175387, 0.41014948399214601, 0.39727260101510603,
+#        0.37019775501297353]
